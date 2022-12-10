3bc83e6d14e5e57b96dd27d25e0cab31795c15f4
MayaRaytan <mayaraytan@mail.tau.ac.il>
2022-12-10 12:17:44 +0200

    "message slot minor changes"

--- mesage_slot.h
+++ mesage_slot.h
@@ -1,6 +1,5 @@
 #define MAJOR_NUMBER = 235
+#define BUFFER_SIZE = 128
 
-int device_search(struct file* file);
 static long device_ioctl(struct file* file, unsigned int ioctl_command_id, unsigned long ioctl_param);
-device *device_files[256];
-int device_files_len;
+device_channels *device_files;
